1d04c83aa20517bf4b5ff49618aa0b4dfebf4249
Mahboob Hussain <mahboob.h06@gmail.com>
2020-04-25 18:35:34 +0530

    Removed unecessary lines

--- cidc.rb
+++ cidc.rb
@@ -1,15 +1,11 @@
 require 'date'
 
-
 arg          = ARGV[0] # in %m-%d-%Y format
 current_date = Date.strptime(arg, "%m-%d-%Y")
 prev_date    = current_date-1
 
-
 path1 = 'datasets/india_daily_delta/'
 path2 = 'datasets/india_daily_cumulative/'
-
-
 
 current_delta_file      = path1 + current_date.strftime("%m-%d-%Y") + '.csv'
 prev_cumulative_file    = path2 + prev_date.strftime("%m-%d-%Y") + '.csv'
@@ -19,13 +15,11 @@ prev_cumulative_file_data    = Hash.new {|hash, key| hash[key] = []}
 output_data                  = Hash.new {|hash, key| hash[key] = []}
 
 File.foreach(current_delta_file) do |line|
-    # puts line
     line = line.split(',')
     current_delta_file_data[line[0]] << line[1].to_i << line[2].to_i << line[3].to_i
 end
 
 File.foreach(prev_cumulative_file) do |line|
-    # puts line
     line = line.split(',')
     prev_cumulative_file_data[line[0]] << line[1].to_i << line[2].to_i << line[3].to_i
 end
