87b80df635cbb00d8ca31e3937932e80040b9172
Mahboob Hussain <mahboob.h06@gmail.com>
2020-10-16 15:32:36 +0530

    Updated posgresql-commands.sql file

--- database/postgres/postgresql-commands.sql
+++ database/postgres/postgresql-commands.sql
@@ -37,10 +37,21 @@ ALTER DATABASE covid19 SET datestyle TO "ISO, MDY";
 # Restart the server after this command
 
 # bulk copy commands for the four tables
-"C:\Program Files\PostgreSQL\12\bin\psql.exe" -h localhost -U postgres -d covid19 -c "SET client_encoding TO 'UTF8';" -c "\copy global_daily_cumulative(date, place, confirmed, deaths, recovered) FROM 'E:\Code\Corona2020\covid19\global_daily_cumulative.csv' DELIMITER ',' CSV;"
+"C:\Program Files\PostgreSQL\13\bin\psql.exe" -h localhost -U postgres -d covid19 -c "SET client_encoding TO 'UTF8';" -c "\copy global_daily_cumulative(date, place, confirmed, deaths, recovered) FROM 'E:\Code\Corona2020\covid19\global_daily_cumulative.csv' DELIMITER ',' CSV;"
 
-"C:\Program Files\PostgreSQL\12\bin\psql.exe" -h localhost -U postgres -d covid19 -c "SET client_encoding TO 'UTF8';" -c "\copy global_daily_delta(date, place, confirmed, deaths, recovered) FROM 'E:\Code\Corona2020\covid19\global_daily_delta.csv' DELIMITER ',' CSV;"
+"C:\Program Files\PostgreSQL\13\bin\psql.exe" -h localhost -U postgres -d covid19 -c "SET client_encoding TO 'UTF8';" -c "\copy global_daily_delta(date, place, confirmed, deaths, recovered) FROM 'E:\Code\Corona2020\covid19\global_daily_delta.csv' DELIMITER ',' CSV;"
 
-"C:\Program Files\PostgreSQL\12\bin\psql.exe" -h localhost -U postgres -d covid19 -c "SET client_encoding TO 'UTF8';" -c "\copy india_daily_cumulative(date, place, confirmed, deaths, recovered) FROM 'E:\Code\Corona2020\covid19\india_daily_cumulative.csv' DELIMITER ',' CSV;"
+"C:\Program Files\PostgreSQL\13\bin\psql.exe" -h localhost -U postgres -d covid19 -c "SET client_encoding TO 'UTF8';" -c "\copy india_daily_cumulative(date, place, confirmed, deaths, recovered) FROM 'E:\Code\Corona2020\covid19\india_daily_cumulative.csv' DELIMITER ',' CSV;"
 
-"C:\Program Files\PostgreSQL\12\bin\psql.exe" -h localhost -U postgres -d covid19 -c "SET client_encoding TO 'UTF8';" -c "\copy india_daily_delta(date, place, confirmed, deaths, recovered) FROM 'E:\Code\Corona2020\covid19\india_daily_delta.csv' DELIMITER ',' CSV;"
+"C:\Program Files\PostgreSQL\13\bin\psql.exe" -h localhost -U postgres -d covid19 -c "SET client_encoding TO 'UTF8';" -c "\copy india_daily_delta(date, place, confirmed, deaths, recovered) FROM 'E:\Code\Corona2020\covid19\india_daily_delta.csv' DELIMITER ',' CSV;"
+
+
+select count(*) from global_daily_cumulative
+select count(*) from global_daily_delta
+select count(*) from india_daily_cumulative
+select count(*) from india_daily_delta
+
+select * from global_daily_cumulative
+select * from global_daily_delta
+select * from india_daily_cumulative
+select * from india_daily_delta
