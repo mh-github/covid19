50122f038888ba70647f49f89ec1676fe7ca7064
Mahboob Hussain <mahboob.hussain@ordwaylabs.com>
2022-04-18 12:37:20 +0530

    Added block to generate india daily cumulative data

--- idd.rb
+++ idd.rb
@@ -7,10 +7,17 @@ data = JSON.parse(HTTParty.get(URL).to_s)
 for state in data.keys
     for date in data[state]['dates'].keys
         if data[state]['dates'][date]['delta']
-            confirmed = data[state]['dates'][date]['delta']['confirmed'] || 0
-            deceased  = data[state]['dates'][date.to_s]['delta']['deceased']  || 0
-            recovered = data[state]['dates'][date.to_s]['delta']['recovered'] || 0
-            puts "#{date},#{state},#{confirmed},#{deceased},#{recovered}"
+            confirmed_delta = data[state]['dates'][date.to_s]['delta']['confirmed'] || 0
+            deceased_delta  = data[state]['dates'][date.to_s]['delta']['deceased']  || 0
+            recovered_delta = data[state]['dates'][date.to_s]['delta']['recovered'] || 0
+            puts "#{date},#{state},#{confirmed_delta},#{deceased_delta},#{recovered_delta}"
         end
+		
+		if data[state]['dates'][date]['total']
+			confirmed_total = data[state]['dates'][date.to_s]['total']['confirmed'] || 0
+			deceased_total  = data[state]['dates'][date.to_s]['total']['deceased']  || 0
+			recovered_total	= data[state]['dates'][date.to_s]['total']['recovered'] || 0
+			puts "#{date},#{state},#{confirmed_total},#{deceased_total},#{recovered_total}"
+		end
     end
 end
